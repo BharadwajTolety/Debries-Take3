585949de729210e321f02810b417276ab77c8a98
Hao Yin <calvinyin1991@gmail.com>
2018-08-08 18:54:37 -0400

    Complete the back functionalitites
    1. Connected "Tutorial" button to the relative screen
    2. Connected "Start" button to the samplescene
    3. Connected "Credits" button to the relative screen
    3. Add basic framework of ExitScreen
    4.

--- Frontend/Debris/Assets/StartScreen.cs
+++ Frontend/Debris/Assets/StartScreen.cs
@@ -7,7 +7,7 @@ public class StartScreen : MonoBehaviour {
 
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("SampleScene");
     }
     public void ExitGame() 
     {
