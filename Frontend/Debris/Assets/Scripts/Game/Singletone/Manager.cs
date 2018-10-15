57f82ab0444e6b595d25948fee4039b0879d1f3e
uttkarsh21 <narayan.u@husky.neu.edu>
2018-10-15 18:34:57 -0400

    map implementation and some algo stuff

---
+++ Frontend/Debris/Assets/Scripts/Game/Singletone/Manager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : Singleton<Manager>
+{
+    public int mySelection = 1;
+    public float brushSize = 4000;
+
+    public List<float> debrisList;
+    public List<float> TimesList;
+}
---
+++ Frontend/Debris/Assets/Scripts/Game/gameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //get the selected edges and contractors
+        OneContractorsOnly("redLine", "txtRed");
+        OneContractorsOnly("blueLine", "txtBlue");
+        OneContractorsOnly("greenLine", "txtGreen");
+    }
+
+    private int OneContractorsOnly(string objName,string txtName)
+    {
+        int num= GameObject.FindGameObjectsWithTag(objName).Length;
+        GameObject.Find(txtName).GetComponent<UnityEngine.UI.Text>().text = num.ToString();
+        return 0;
+    }
+
+}
