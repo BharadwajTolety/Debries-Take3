1c0a204e0363b205d6296766d2cd5fad6f7dc7f6
Barru <acebharadwaj907@gmail.com>
2018-10-21 22:10:35 -0400

    Ui changes part 1

---
+++ Frontend/Debris/Assets/IconSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconSwitch : MonoBehaviour {
+	public Sprite OffSprite;
+    public Sprite OnSprite;
+    public B but;
+    public void ChangeImage()
+    {
+        if (but.image.sprite == OnSprite)
+            but.image.sprite = OffSprite;
+        else
+        {
+            but.image.sprite = OnSprite;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
