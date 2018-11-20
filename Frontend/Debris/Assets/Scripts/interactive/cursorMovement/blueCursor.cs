e9e3301adf2497d0f9b82e443471b036435e79d4
uttkarsh21 <narayan.u@husky.neu.edu>
2018-11-20 12:43:07 -0500

    interactive scripts be back

---
+++ Frontend/Debris/Assets/Scripts/interactive/cursorMovement/blueCursor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blueCursor : MonoBehaviour {
+  private Vector3 mousePosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	
+        void Update () {
+
+		Vector3 newSize = new Vector3 (Manager.Instance.brushSize, Manager.Instance.brushSize, 1);
+
+          float moveSpeed = 1f;
+            if (Manager.Instance.mySelection==3) {
+              mousePosition = Input.mousePosition;
+              mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+              transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+			  transform.localScale = newSize;
+            }
+
+	}
+
+	/*void OnTriggerEnter2D(Collider2D col){
+		Debug.Log (col.gameObject.tag.ToString ());
+	}
+	*/
+}
---
+++ Frontend/Debris/Assets/Scripts/interactive/cursorMovement/redCursor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class redCursor : MonoBehaviour {
+  private Vector3 mousePosition;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 newSize = new Vector3 (Manager.Instance.brushSize, Manager.Instance.brushSize, 1);
+      float moveSpeed = 1f;
+        if (Manager.Instance.mySelection==1) {
+			mousePosition = Input.mousePosition;
+			mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            
+
+
+            
+           if (this.tag == "txtSelected")
+            {
+                mousePosition.Set(mousePosition.x-250, mousePosition.y-250, mousePosition.z);
+                transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+                //transform.localScale = 1.0f;
+            }
+            else
+            {
+                transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+                transform.localScale = newSize;
+            }
+        }
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+
+		Debug.Log ("ok");
+
+	}
+
+}
