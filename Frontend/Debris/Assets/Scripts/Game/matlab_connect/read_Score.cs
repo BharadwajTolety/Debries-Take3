b52dd95e4d3dd871de58731751bd8853808d6e4d
uttkarsh21 <narayan.u@husky.neu.edu>
2018-12-19 13:33:00 -0500

    read them scores

---
+++ Frontend/Debris/Assets/Scripts/Game/matlab_connect/read_Score.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+public class read_Score : MonoBehaviour
+{
+    StreamReader readScore;
+    string scorePath;
+    float minTime, maxProfit;
+
+    private void Awake()
+    {
+        scorePath = Application.dataPath + "/Database/Input/score_info_fromMatlab.txt";
+        
+        reset_score();
+    }
+
+    void reset_score()
+    {
+        if(!File.Exists(scorePath))
+        {
+            File.CreateText(scorePath).Dispose();
+        }
+        //StreamWriter writeScore = new StreamWriter(scorePath);
+        //reset/init the file to waiting..
+        File.WriteAllText(scorePath,"waiting...");
+
+        //reset/init variables
+        minTime = 0; maxProfit = 0;
+    }
+
+    bool read_score()
+    {
+        string initRead;
+
+        try
+        {
+            initRead = File.ReadAllText(scorePath);            
+        }
+        catch (Exception e)
+        {
+            Debug.Log("the file couldnt be read - " + e.Message);
+            return false;
+        }
+
+        if (initRead == "waiting...")
+        {
+            return false;
+        }
+
+        string[] scoreInfo = new string[2];
+
+        scoreInfo = initRead.Split(',');
+
+        maxProfit = float.Parse(scoreInfo[0]);
+        minTime = float.Parse(scoreInfo[1]);
+        return true;
+    }
+
+    public void reading()
+    {
+        Debug.Log("waiting on score...");
+        //while (!read_score()) ;
+        read_score();
+        Debug.Log(maxProfit);
+        Debug.Log(minTime);
+    }
+}
