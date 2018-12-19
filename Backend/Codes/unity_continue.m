22fada79edb5347ad1459c0e6174b05216df2a69
uttkarsh21 <narayan.u@husky.neu.edu>
2018-12-19 15:28:01 -0500

    backend update

---
+++ Backend/Codes/unity_continue.m
@@ -0,0 +1,10 @@
+%% this to be run when the game is already running.
+%% update problem data edgelist.
+
+%% run main
+clear X
+X = importdata('C:/Users/Uttkarsh/Desktop/Debris_work_folder/work_debris/Frontend/Debris/Assets/Database/Output/edgelist_forMatlab.csv');
+
+EdgeList = X;
+
+Main3(); 
