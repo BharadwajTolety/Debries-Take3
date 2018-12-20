ded4da556d07806e83aaa5be5dfd10b414b55c40
uttkarsh21 <narayan.u@husky.neu.edu>
2018-12-20 13:48:34 -0500

    map transformation initial stuff

---
+++ Frontend/Debris/Assets/Scripts/MapAssembly/map_transformations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class map_transformations : MonoBehaviour {
+    
+    [SerializeField]
+    private Vector3 rot = new Vector3(0, 0, -61), pos = new Vector3(-53, 803, 0);
+    [SerializeField]
+    private float zoom = 0.05f;
+
+    private void Start()
+    {
+        rotate_to_rectangle();
+    }
+
+    private void rotate_to_rectangle()
+    {
+        transform.Rotate(rot);
+        transform.position = pos;
+    }
+
+    public void zoom_in()
+    {
+        transform.localScale += new Vector3(zoom, zoom, zoom);
+    }
+
+    public void zoom_out()
+    {
+        transform.localScale -= new Vector3(zoom, zoom, zoom);
+    }
+}
