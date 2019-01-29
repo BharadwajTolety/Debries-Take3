59087957945c5844581d6e65b22bd848eb56467e
uttkarsh21 <narayan.u@husky.neu.edu>
2019-01-29 15:18:17 -0500

    make edges blink instead of nodes

--- Frontend/Debris/Assets/Scripts/interactive/badEdge_blinkers.cs
+++ Frontend/Debris/Assets/Scripts/interactive/badEdge_blinkers.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class badEdge_blinkers : MonoBehaviour {
 
-    private List<int> bad_nodes = new List<int>();
+    private List<string> bad_edges = new List<string>();
     private bool on = true;
     private string badEdge_path;
 
@@ -26,7 +26,7 @@ public class badEdge_blinkers : MonoBehaviour {
             File.Delete(badEdge_path);
         }
 
-        bad_nodes = new List<int>();
+        bad_edges = new List<string>();
     }
 
     //read the badEdge file after matlab done writing it
@@ -48,12 +48,12 @@ public class badEdge_blinkers : MonoBehaviour {
                     string[] bE_info = new string[2];
                     bE_info = badEdge.Split(',');
 
-                    int[] badEdge_info = { int.Parse(bE_info[0]), int.Parse(bE_info[1]) };
+                    string badEdge_info = bE_info[0] + "_" + bE_info[1];
 
-                    if (!bad_nodes.Contains(badEdge_info[0]))
-                        bad_nodes.Add(badEdge_info[0]);
-                    if (!bad_nodes.Contains(badEdge_info[1]))
-                        bad_nodes.Add(badEdge_info[1]);
+                    if (!bad_edges.Contains(badEdge_info))
+                    {
+                        bad_edges.Add(badEdge_info);
+                    }
                 }
 
                 Debug.Log("Done reading badEdge file");
@@ -72,45 +72,58 @@ public class badEdge_blinkers : MonoBehaviour {
     {
         //read_badEdges();
         on = toggle.GetComponent<Toggle>().isOn;
-        if(bad_nodes != null)
+        if(bad_edges != null)
             StartCoroutine(blinkers());
     }
 
     public IEnumerator blinkers()
     {
-        GameObject[] nodes = GameObject.FindGameObjectsWithTag("objNode");
-        Vector3 wobble = new Vector3(.3f, .3f, 0);
+        GameObject[] redLine = GameObject.FindGameObjectsWithTag("red");
+        GameObject[] greenLine = GameObject.FindGameObjectsWithTag("green");
+        GameObject[] blueLine = GameObject.FindGameObjectsWithTag("blue");
 
+        //Queue<string> badEdges = new Queue<string>();
         while(on)
         {
-           foreach (GameObject node in nodes)
+            foreach (string bad_edge in bad_edges)
             {
-                foreach (int bad_node in bad_nodes)
-                {
-                    if (node.name.Contains("n_" + bad_node))
-                    {
-                        node.GetComponent<SpriteRenderer>().color = Color.yellow;
-                        node.GetComponent<Transform>().localScale += wobble;
-                        break;
-                    }                   
-                }
+                update_bad(redLine, bad_edge, true);
+                update_bad(greenLine, bad_edge, true);
+                update_bad(blueLine, bad_edge, true);
             }
 
-            yield return new WaitForSeconds(.5f);
-           
-            foreach (GameObject node in nodes)
+            yield return new WaitForSeconds(0.5f);
+
+            foreach (string bad_edge in bad_edges)
             {
-                foreach (int bad_node in bad_nodes)
+                update_bad(redLine, bad_edge, false);
+                update_bad(greenLine, bad_edge, false);
+                update_bad(blueLine, bad_edge, false);
+            }
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void update_bad(GameObject[] lines, string bad_edge, bool on)
+    {
+        Vector3 wobble = new Vector3(.3f, .3f, 0);
+
+        foreach (GameObject line in lines)
+        {
+            if (line.name.EndsWith("_" + bad_edge))
+            {
+                if(on)
                 {
-                    if (node.name.Contains("n_" + bad_node))
-                    {
-                        node.GetComponent<SpriteRenderer>().color = Color.white;
-                        node.GetComponent<Transform>().localScale -= wobble;
-                        break;
-                    }
+                    line.GetComponent<SpriteRenderer>().color = Color.yellow;
+                    line.GetComponent<Transform>().localScale += wobble;
+                }
+                else
+                {
+                    line.GetComponent<SpriteRenderer>().color = Color.white;
+                    line.GetComponent<Transform>().localScale -= wobble;
                 }
             }
-            yield return new WaitForSeconds(0.5f);
         }
     }
 }
