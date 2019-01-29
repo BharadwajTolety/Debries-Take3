a701278385e0c959b529c017681e1834dccb0b8c
uttkarsh21 <narayan.u@husky.neu.edu>
2019-01-29 15:09:03 -0500

    remove graph view and scan input controls

--- Frontend/Debris/Assets/Scripts/interactive/graph_view.cs
+++ Frontend/Debris/Assets/Scripts/interactive/graph_view.cs
@@ -10,176 +10,24 @@ public class graph_view : MonoBehaviour {
     GameObject intersect_view;
     GameObject mapscreen;
 
-    bool profit_active, time_active, intersect_active;
-
+    //the name doesn't make sense cause the functionality has been deprecated but it works on scan_disable stuff so it's still here.
     private void Awake()
     {
-        profit_view = GameObject.Find("profit_charts");
-        time_view = GameObject.Find("time_charts");
         mapscreen = GameObject.Find("MapScreen");
-        intersect_view = GameObject.Find("intersect_view");
 
-        GameObject.Find("notification_whiteLines").SetActive(false);
-        profit_active = false;
-        time_active = false;
-        intersect_active = false;
         scan_disable();
     }
 
-    public void toggle_view(bool profit)
-    {
-        if (profit)
-            if (!profit_active)
-            {
-                profit_view.transform.position = new Vector3(0, 0, 0);
-                intersect_view.transform.position = new Vector3(251, 1188, 0);
-                time_view.transform.position = new Vector3(-2554, 0, 0);
-                mapscreen.SetActive(false);
-
-                profit_active = true;
-                time_active = false;
-                intersect_active = false;
-            }
-            else
-            {
-                profit_view.transform.position = new Vector3(1957, 0, 0);
-                time_view.transform.position = new Vector3(-2554, 0, 0);
-                intersect_view.transform.position = new Vector3(251, 1188, 0);
-                mapscreen.SetActive(true);
-
-                time_active = false;
-                intersect_active = false;
-                profit_active = false;
-            }
-        else
-            if (!time_active)
-            {
-                profit_view.transform.position = new Vector3(1957, 0, 0);
-                time_view.transform.position = new Vector3(0, 0, 0);
-                intersect_view.transform.position = new Vector3(251, 1188, 0);
-                mapscreen.SetActive(false);
-                
-                time_active = true;
-                profit_active = false;
-                intersect_active = false;
-            }
-            else
-            {
-                profit_view.transform.position = new Vector3(1957, 0, 0);
-                time_view.transform.position = new Vector3(-2554, 0, 0);
-                intersect_view.transform.position = new Vector3(251, 1188, 0);
-                mapscreen.SetActive(true);
-                
-                time_active = false;
-                intersect_active = false;
-                profit_active = false;
-            }
-    }
-
-    public void toggle_intersect()
-    {
-        if (!intersect_active)
-        {
-            intersect_active = true;
-            time_active = false;
-            profit_active = false;
-
-            intersect_view.transform.position = new Vector3(251, 65, 0);
-            profit_view.transform.position = new Vector3(1957, 0, 0);
-            time_view.transform.position = new Vector3(-2554, 0, 0);
-
-            GameObject red_text = GameObject.Find("red_text");
-            GameObject blue_text = GameObject.Find("blue_text");
-            GameObject green_text = GameObject.Find("green_text");
-
-            GameObject[] themRedEdges = GameObject.FindGameObjectsWithTag("redLine");
-            GameObject[] themBlueEdges = GameObject.FindGameObjectsWithTag("blueLine");
-            GameObject[] themGreenEdges = GameObject.FindGameObjectsWithTag("greenLine");
-
-            List<int> node = new List<int>();
-            string[] nodeInfo = new string[4];
-
-            for (int i = 0; i < themRedEdges.Length; i++)
-            {
-                nodeInfo = themRedEdges[i].name.Split('_');
-                if (nodeInfo.Length > 2)
-                {
-                    if(!node.Contains(int.Parse(nodeInfo[2])))
-                        node.Add(int.Parse(nodeInfo[2]));
-
-                    if(!node.Contains(int.Parse(nodeInfo[3])))
-                        node.Add(int.Parse(nodeInfo[3]));
-
-                    red_text.GetComponent<Text>().text = node.Count.ToString();
-                }
-            }
-
-            node.Clear();
-            for (int i = 0; i < themGreenEdges.Length; i++)
-            {
-                nodeInfo = themGreenEdges[i].name.Split('_');
-                if (nodeInfo.Length > 2)
-                {
-                    if (!node.Contains(int.Parse(nodeInfo[2])))
-                        node.Add(int.Parse(nodeInfo[2]));
-
-                    if (!node.Contains(int.Parse(nodeInfo[3])))
-                        node.Add(int.Parse(nodeInfo[3]));
-
-                    green_text.GetComponent<Text>().text = node.Count.ToString();
-                }
-            }
-
-            node.Clear();
-            for (int i = 0; i < themBlueEdges.Length; i++)
-            {
-                nodeInfo = themBlueEdges[i].name.Split('_');
-                if (nodeInfo.Length > 2)
-                {
-                    if (!node.Contains(int.Parse(nodeInfo[2])))
-                        node.Add(int.Parse(nodeInfo[2]));
-
-                    if (!node.Contains(int.Parse(nodeInfo[3])))
-                        node.Add(int.Parse(nodeInfo[3]));
-
-                    blue_text.GetComponent<Text>().text = node.Count.ToString();
-                }
-            }
-
-            mapscreen.SetActive(false);
-        }
-        else
-        {
-            profit_view.transform.position = new Vector3(1957, 0, 0);
-            time_view.transform.position = new Vector3(-2554, 0, 0);
-            intersect_view.transform.position = new Vector3(251, 1188, 0);
-            mapscreen.SetActive(true);
-
-            time_active = false;
-            intersect_active = false;
-            profit_active = false;
-        }
-    }
-
-    public void toggle_noti(GameObject notification)
+    public void toggle_noti()
     {
         GameObject[] themWhiteLines = GameObject.FindGameObjectsWithTag("whiteLine");
         if (themWhiteLines.Length > 1)
         {
-            notification.SetActive(true);
             scan_disable();
         }
         else
         {
             GameObject.Find("Toggle").GetComponent<Toggle>().interactable = true;
-            //GameObject.Find("scanning_notification").GetComponent<SpriteRenderer>().enabled = true;
-
-            //not the most elegant way to make sure everything is on screen but hey it works = wait till the notification is on the screen then read contractor info
-            //do
-            //{
-           //     if (GameObject.Find("scanning_notification").GetComponent<SpriteRenderer>().isVisible)
-            //        GameObject.Find("GameManager").GetComponent<contInfo_Matlab>().read_contractor_info();
-           // } //while (!GameObject.Find("scanning_notification").GetComponent<SpriteRenderer>().isVisible);
         }
     }
 
---
+++ Frontend/Debris/Assets/Scripts/interactive/scan_controls.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scan_controls : MonoBehaviour
+{
+    public Button profit_button, time_button, intersect_button;
+    public Sprite[] profit_sprite, time_sprite, intersect_sprite;
+    private int profit_obj, time_obj, intersect_obj, scan;
+
+    private void Awake()
+    {
+        profit_obj = 0;
+        time_obj = 0;
+        intersect_obj = 0;
+        scan = 0;
+    }
+
+    public void profit_check()
+    {
+        if (profit_obj == 1)
+            profit_obj = 0;
+        else
+            profit_obj = 1;
+
+        update_button(profit_button, profit_sprite, profit_obj);
+    }
+
+    public void time_check()
+    {
+        if (time_obj == 1)
+            time_obj = 0;
+        else
+            time_obj = 1;
+
+        update_button(time_button, time_sprite, time_obj);
+    }
+
+    public void intersect_check()
+    {
+        if (intersect_obj == 1)
+            intersect_obj = 0;
+        else
+            intersect_obj = 1;
+
+        update_button(intersect_button, intersect_sprite, intersect_obj);
+    }
+
+    public void scan_check()
+    {
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<contInfo_Matlab>().read_contractor_info(profit_obj, time_obj, intersect_obj);
+    }
+
+    private void update_button(Button butt, Sprite[] spri, int pressed)
+    {
+        if (pressed == 1)
+            butt.GetComponent<Image>().sprite = spri[0];
+        else
+            butt.GetComponent<Image>().sprite = spri[1];
+    }
+}
